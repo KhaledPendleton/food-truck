f84dd839f0e576db1341bf6b7e73c8d65832bcdb
Khaled Pendleton <hi@khaledpendleton.com>
2023-03-02 15:30:33 -0500

    Installed CSV management library

--- apps/food_truck/mix.exs
+++ apps/food_truck/mix.exs
@@ -42,7 +42,8 @@ defmodule FoodTruck.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.2"},
       {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"}
+      {:finch, "~> 0.13"},
+      {:csv, "~> 3.0"}
     ]
   end
 
