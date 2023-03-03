5613d85135105782559c3ffbd67e5aaa6aa7f72a
Khaled Pendleton <hi@khaledpendleton.com>
2023-03-03 01:07:29 -0500

    Company has many associated locations

--- apps/food_truck/lib/food_truck/search/company.ex
+++ apps/food_truck/lib/food_truck/search/company.ex
@@ -1,9 +1,12 @@
 defmodule FoodTruck.Search.Company do
   use Ecto.Schema
   import Ecto.Changeset
+  alias FoodTruck.Search
 
   schema "companies" do
     field :name, :string
+
+    has_many :locations, Search.Location
 
     timestamps()
   end
@@ -13,5 +16,6 @@ defmodule FoodTruck.Search.Company do
     company
     |> cast(attrs, [:name])
     |> validate_required([:name])
+    |> cast_assoc(:locations, with: &Search.Location.changeset/2)
   end
 end
