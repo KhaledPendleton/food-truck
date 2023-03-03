0cbe5dc2c32e720edb084d1ca688504cedd4140d
Khaled Pendleton <hi@khaledpendleton.com>
2023-03-03 00:13:41 -0500

    Created companies db table

---
+++ apps/food_truck/priv/repo/migrations/20230303051117_create_companies.exs
@@ -0,0 +1,11 @@
+defmodule FoodTruck.Repo.Migrations.CreateCompanies do
+  use Ecto.Migration
+
+  def change do
+    create table(:companies) do
+      add :name, :string
+
+      timestamps()
+    end
+  end
+end
